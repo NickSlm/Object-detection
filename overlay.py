50f621dfd5263d8fcbe9591dd3ce71bb9b66fa51
NickSlm <solomonovnick@gmail.com>
2022-02-15 15:47:20 +0200

    modified overlay class

--- overlay.py
+++ overlay.py
@@ -1,38 +1,33 @@
 from ntpath import join
 from PyQt5 import QtCore,QtWidgets,QtGui
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QMainWindow,QPushButton,QVBoxLayout,QWidget,QGridLayout
-from PyQt5.QtGui import QPainter, QBrush, QPen,QColor
+from PyQt5.QtWidgets import QMainWindow,QPushButton,QVBoxLayout,QWidget,QScrollArea,QFormLayout, QGroupBox,QGridLayout
+from PyQt5.QtGui import QPainter, QBrush, QPen,QColor,QIcon
 import sys
 from image_detection import image_detect
 from utils import get_recipes
 
-class Overlay(QMainWindow):
+class Overlay(QWidget):
     def __init__(self):
         super().__init__()
-        self.init_window()
+        self.left = 0
+        self.top = 0
+        self.w = 1096
+        self.h = 1024
 
-    def init_window(self):
-        # MAKE WIDOW TRANSPARENT
-        # self.setAttribute(Qt.WA_TransparentForMouseEvents, True)
-        # self.setAttribute(Qt.WA_NoChildEventsForParent, True)
         self.setWindowFlags(Qt.Window|Qt.X11BypassWindowManagerHint|Qt.WindowStaysOnTopHint|Qt.FramelessWindowHint)
         self.setAttribute(Qt.WA_TranslucentBackground)
-
-        self.setGeometry(0,0,1920,1024)
+        self.setGeometry(self.left,self.top,self.w,self.h)
         self.showMaximized()
 
-        self.setCentralWidget(self.create_layout())
 
-    def create_layout(self):
-        self.central_widget = QWidget()
-        self.vlayout = QVBoxLayout()
-        self.glayout = QGridLayout()
+        form_layout = QFormLayout()
+        group_box = QGroupBox()
 
-        # Create Buttons
-        recipes_data = get_recipes()
+        recipes_data = get_recipes()        
         for recipe in recipes_data.keys():
             self.recipe = QPushButton()
+            self.recipe.clicked.connect(self.select_recipe)
             self.recipe.setStyleSheet("QPushButton"
                             "{"
                             f"border-image: url({recipes_data[recipe]['icon']});"
@@ -49,13 +44,24 @@ class Overlay(QMainWindow):
                             )
             self.recipe.setToolTip(f"{recipe}\n{', '.join(recipes_data[recipe]['drops'])}")
             self.recipe.setMinimumSize(48,48)
-            self.recipe.setMaximumSize(48,48)    
-            self.recipe.clicked.connect(self.select_recipe)
-            self.vlayout.addWidget(self.recipe,alignment=QtCore.Qt.AlignCenter | QtCore.Qt.AlignTop)
-        self.central_widget.setLayout(self.vlayout)
+            self.recipe.setMaximumSize(48,48) 
+            form_layout.addRow(self.recipe)
 
-        return self.central_widget
+        group_box.setLayout(form_layout)
+        scroll = QScrollArea()
+        scroll.setWidget(group_box)
+        scroll.setWidgetResizable(True)
+        scroll.setFixedHeight(400)
+        scroll.setFixedWidth(96)
 
+        layout = QGridLayout()
+        layout.addWidget(scroll,0,0,QtCore.Qt.AlignTop)
+
+        self.setLayout(layout)
+
+        self.show()
+
+        
     def keyPressEvent(self, e):
         # Close overlay
         if e.key() == Qt.Key_F8:
@@ -66,7 +72,7 @@ class Overlay(QMainWindow):
         self.mark_objects(painter)
 
     def mark_objects(self,painter):
-        s_x,s_y,e_x,e_y = image_detect(r'D:\Find image on screen\images\templates\ModRecipeCorpseeploder.png')
+        s_x,s_y,e_x,e_y = image_detect(r'D:\Find image on screen\images\templates\ModRareKillEnergyShield.png')
         painter.setPen(QPen(Qt.yellow, 3, Qt.SolidLine))
         painter.drawRect(s_x,s_y, 48 ,48)
 
